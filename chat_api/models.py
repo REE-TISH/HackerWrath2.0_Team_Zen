5f87fc2fd7b168a4e3b89eddf0c2718ca35a2eb4
REE_TISH <reetishk798@gmail.com>
2025-11-03 17:16:00 +0530

    added stuff

--- chat_api/models.py
+++ chat_api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class EachUserChat(models.Model):
+    user_id = models.CharField(max_length=100, unique=True)
+    chat_history = models.JSONField(default=list)
+
+    def __str__(self):
+        return self.user_id
